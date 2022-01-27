07cf38bd2da6a3667e8db4d706cea7784187492f
nikakoy-png <64211546+nikakoy-png@users.noreply.github.com>
2022-01-28 00:30:48 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ from db import *
 
 db = DB('mongodb://localhost:27017', 'CommPot')
 KEY_BOT = "5135434281:AAGcbkNhqffgzRnNbr-sqRLTC1TEjgspJQY"
-filepath = 'C:/Users/nikit/PycharmProjectsCumShot/photos/'
+filepath = '/home/user/tmp/photos/'
