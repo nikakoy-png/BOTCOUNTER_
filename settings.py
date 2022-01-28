aa5bf527d8c79c64445d11efadd3662e72b745c8
nikakoy-png <64211546+nikakoy-png@users.noreply.github.com>
2022-01-28 23:00:43 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ from db import *
 
 db = DB("mongodb+srv://nikakoy_:Bb190920031209@cluster0.yuufd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 'CommPot')
 KEY_BOT = "5135434281:AAGcbkNhqffgzRnNbr-sqRLTC1TEjgspJQY"
-filepath = '/root/BOTCOUNTER_/photos/'
+filepath = '/root/FINAL_BOT/photos/'
