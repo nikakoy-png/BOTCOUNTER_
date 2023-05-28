cc79a0b020a36b58e9195b731664f6e23acdfd59
Bilohur += Mykyta <64211546+nikakoy-png@users.noreply.github.com>
2023-05-29 00:04:58 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ from lib import *
 from db import *
 
 db = DB("mongodb+srv://nikakoy_:Bb190920031209@cluster0.yuufd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 'CommPot')
-KEY_BOT = "5135434281:AAGcbkNhqffgzRnNbr-sqRLTC1TEjgspJQY"
+KEY_BOT = ""
 filepath = '/root/FINAL_BOT/photos/'
